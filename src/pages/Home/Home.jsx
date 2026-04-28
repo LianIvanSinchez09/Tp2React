9655949578b2f71714cd1b2e84f2aa736929cd81
lianIvanSinchez <maquiavelic1030@gmail.com>
2026-04-28 20:57:31 -0300

    merge de los cambios de nacho + itemcard y loader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="grow">
-        Home 
+         Home  
       </main>
       <Footer />
     </div>
