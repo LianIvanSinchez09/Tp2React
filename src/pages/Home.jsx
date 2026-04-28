1f146f90fe11b5d3981016f20c64b19b21ee6784
lianIvanSinchez <maquiavelic1030@gmail.com>
2026-04-28 20:39:45 -0300

    añadido itemcard y home para testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { getProducts } from '../services/api';
+import ItemCard from '../Components/ItemCard/ItemCard.jsx';
+import ItemLoader from '../Components/ItemLoader/ItemLoader.jsx';
 
 const Home = () => {
-  // const [products, setProducts] = useState([]); 
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   getProducts()
-  //     .then(data => {
-  //       setProducts(data);
-  //       setLoading(false);
-  //     })
-  //     .catch(err => {
-  //       setError(err.message);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  // if (loading){
-  //    return <p>Cargando...</p>
-  // };
-  // if (error){   
-  //   return <p>Error: {error}</p>
-  // };
-
-  // return (
-  //   <div>
-  //     {products.length > 0
-  //       ? products.map((item) => (
-  //           <div key={item.id}>
-  //             {item?.name}
-  //           </div>
-  //         ))
-  //       : "No hay productos"
-  //     }
-  //   </div>
-  // );
+  return (
+    <>
+      <p>Home</p>
+      <ItemLoader/>
+    </>
+  )
 }
 
 export default Home;
