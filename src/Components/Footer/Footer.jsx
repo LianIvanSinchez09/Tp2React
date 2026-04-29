a90bcae8a606f255d8360e50355130106d2be0e9
Naxo <joaquin.castillo@est.fi.uncoma.edu.ar>
2026-04-29 17:36:45 -0300

    Incorporacion de rutas, footer, header adedmas de arreglos de bugs y demas

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,23 +1,29 @@
+import { useTranslation } from "react-i18next";
+
 export const Footer = () => {
   const integrantes = [
-    "Lian Ivan Sinchez",
-    "Lucas San Segundo",
-    "Joaquin Ignacio Castillo"
+    { nombre: "Lian Ivan Sinchez", link: "https://github.com/LianIvanSinchez09" },
+    { nombre: "Lucas San Segundo", link: "https://github.com/Lucasanse" },
+    { nombre: "Joaquin Ignacio Castillo", link: "https://github.com/NaxoCastt" }
   ];
-
+  const { t } = useTranslation();
   return (
-    <footer className="bg-gray-900 text-gray-100 py-6">
-      <div className="container mx-auto px-6">
-        <h1 className="text-2xl font-bold mb-2">Trabajo práctico 2</h1>
-        <h2 className="text-xl font-semibold mb-4">Integrantes:</h2>
-        <div className="flex flex-col gap-2">
+    <footer className="h-min from-pink-200 via-purple-200 to-blue-200 text-gray-900 py-8 border-t-4 border-dashed border-white">
+      <div className="container mx-auto px-6 justify-center items-center">
+        <h2 className="text-xl font-semibold mb-4 text-blue-700">
+          {t("footer.integers")}
+        </h2>
+        <div className="flex flex-col gap-3 w-full items-center">
           {integrantes.map((e) => (
-            <p
-              key={e}
-              className="text-lg bg-gray-800 px-4 py-2 rounded-md shadow hover:bg-gray-700 transition"
+            <a
+              key={e.nombre}
+              href={e.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-lg w-xl px-4 py-2 rounded-lg bg-white border-l-4 border-purple-400 shadow hover:scale-105 hover:border-pink-400 transition-transform duration-200"
             >
-              {e}
-            </p>
+              {e.nombre}
+            </a>
           ))}
         </div>
       </div>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router";
+import { LanguageSwitcher } from "../LanguageSwitcher/LanguageSwitcher";
+import { useTranslation } from "react-i18next";
+
+export const Header = () => {
+  const { t } = useTranslation();
+
+  return (
+    <header className=" from-pink-200 via-purple-200 to-blue-200 shadow-[0_0_20px_rgba(0,0,0,0.15)] border-b-4 border-dashed border-white">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        
+        {/* Logo */}
+        <div className="text-2xl font-bold text-purple-700 flex items-center drop-shadow">
+          <NavLink to="/">
+            <img className="w-20" src="logo.png" alt="Logo de IUPI" />
+          </NavLink>
+        </div>
+
+        {/* Navegación */}
+        <nav className="flex gap-6 font-medium">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "text-purple-700 font-bold border-b-2 border-purple-400 transition text-3xl"
+                : "text-gray-700 hover:text-pink-500 transition text-3xl"
+            }
+          >
+            {t("header.inicio")}
+          </NavLink>
+
+          <NavLink
+            to="/detalles"
+            className={({ isActive }) =>
+              isActive
+                ? "text-purple-700 font-bold border-b-2 border-purple-400 transition text-3xl"
+                : "text-gray-700 hover:text-pink-500 transition text-3xl"
+            }
+          >
+            {t("header.detalles")}
+          </NavLink>
+        </nav>
+
+        {/* Selector de idioma */}
+        <div className="ml-4">
+          <LanguageSwitcher />
+        </div>
+      </div>
+    </header>
+  );
+};
