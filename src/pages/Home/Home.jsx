750cfe98943d51f3a603f7da964563ce9552731d
Naxo <joaquin.castillo@est.fi.uncoma.edu.ar>
2026-04-28 22:42:37 -0300

    Incorporacion primera parte de traducciones

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Footer } from '../../Components/Footer/Footer'
+import { useTranslation } from "react-i18next"
 
 const Home = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col min-h-screen">
       <main className="grow">
-        Home 
+        <h1>{t("home.title")}</h1>
       </main>
       <Footer />
     </div>
