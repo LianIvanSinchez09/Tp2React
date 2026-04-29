6ce0be4f2ed4f0b833fa26eaf7005ae015c7a87b
Naxo <joaquin.castillo@est.fi.uncoma.edu.ar>
2026-04-28 22:57:48 -0300

    prueba

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const Footer = () => {
   const integrantes = [
     "Lian Ivan Sinchez",
@@ -23,20 +22,5 @@ export const Footer = () => {
         </div>
       </div>
     </footer>
-=======
-export const Footer = ({ integrantes }) => {
-  return (
-    <div className="container p-10">
-      <h1 className="text-xl">Trabajo practico 2</h1>
-      <h2 className="text-xl">Integrantes:</h2>
-      <div className="flex flex-col">
-        {integrantes.map((e) => (
-          <h3 className="lg" key={e}>
-            {e}
-          </h3>
-        ))}
-      </div>
-    </div>
->>>>>>> Rama-de-Naxo
   );
 };
